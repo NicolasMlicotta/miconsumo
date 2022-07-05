dd48376866b3ed4499c009ea7c0c3b242aa9d525
Nicolás Mlicotta <nicolasmlicottadeveloper@gmail.com>
2022-07-04 23:20:13 -0300

    search bar in progress

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,14 +2,29 @@ import { useState } from "react";
 import { AiFillWindows, AiOutlineSearch } from "react-icons/ai";
 import styles from "./SearchBar.module.css";
 
-function SearchBar({ showInput = true, width, placeholder }) {
+function SearchBar({
+  showInput = true,
+  width = "40ch",
+  placeholder,
+  array = ["corona", "budweiser", "quilmes", "brahma"],
+}) {
   const decoration = { width: width };
   const [showBar, setShowBar] = useState(showInput);
-  const handleSearch = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchArray, setSearchArray] = useState(array);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    if (e.target.value === "") {
+      setSearchArray(array);
+    } else handleSearch(e.target.value);
+  };
+  const handleSearch = (value = searchValue) => {
     if (!showBar) {
       setShowBar(true);
     } else {
-      alert("holis");
+      let filtered = searchArray.filter((item) => item.includes(value));
+      setSearchArray(filtered);
     }
   };
   return (
@@ -20,6 +35,8 @@ function SearchBar({ showInput = true, width, placeholder }) {
           className={styles.textInput}
           placeholder={placeholder ?? "Buscar"}
           style={decoration}
+          value={searchValue}
+          onChange={handleChange}
         />
       )}
       <AiOutlineSearch
@@ -27,6 +44,11 @@ function SearchBar({ showInput = true, width, placeholder }) {
         className={styles.searchIcon}
         onClick={handleSearch}
       />
+      <ul>
+        {searchArray.map((val, index) => (
+          <li key={index}>{val}</li>
+        ))}
+      </ul>
     </div>
   );
 }
